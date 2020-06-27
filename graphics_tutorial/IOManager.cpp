79a72bfa6ec82879a1cb8b2d16b751fa928bd03d
Marcin Lesniewski <lesniakmeister@gmail.com>
2020-06-27 22:34:50 +0200

    End

--- graphics_tutorial/IOManager.cpp
+++ graphics_tutorial/IOManager.cpp
@@ -2,7 +2,7 @@
 
 #include <fstream>
 
-bool IOManager::readFileToBuffer(std::string filePath, std::vector<char>& buffer)
+bool IOManager::readFileToBuffer(std::string filePath, std::vector<unsigned char>& buffer)
 {
 	std::ifstream file(filePath, std::ios::binary);
 	if (file.fail()) {
@@ -23,7 +23,7 @@ bool IOManager::readFileToBuffer(std::string filePath, std::vector<char>& buffer
 	fileSize -= file.tellg();
 
 	buffer.resize(fileSize);
-	file.read(&(buffer[0]),fileSize);
+	file.read((char*)&(buffer[0]),fileSize);              //udajemy char* poniewaz mieszamy unsigned z signed ze wzgledu ze pracujemy na binary
 	file.close();
 
 	return true;
---
+++ graphics_tutorial/ImageLoader.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "GLTexture.h"
+
+#include <string>
+
+class ImageLoader
+{
+public:
+	GLTexture loadPNG(std::string filePath);
+	
+};
+
